3b8a135f84d02a0ccc2292d8bbec9a5c489e6918
vishwa-zee <vishwav.rc@gmail.com>
2022-04-15 11:13:52 +0530

    Added collection view with sample cell

--- Yuppies Tech/Yuppies Tech/Views/ViewController.swift
+++ Yuppies Tech/Yuppies Tech/Views/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Constants {
+        static let cellIdentifier = "identifier"
+    }
 
+    @IBOutlet weak var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: Constants.cellIdentifier,
+            for: indexPath)
+        cell.backgroundColor = UIColor.green
+        return cell
+    }
+}
